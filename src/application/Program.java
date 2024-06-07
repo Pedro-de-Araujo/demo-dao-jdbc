ca1e012523d59539eeb36d50a87f23c19acfee5e
Pedro de Araujo <pedro0306araujo@gmail.com>
2024-06-06 22:34:48 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -12,24 +13,28 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: seller findByDepartment ===");
-		Department department = new Department(1, null);
+		Department department = new Department(3, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		for(Seller obj: list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
 		System.out.println("\n=== TEST 3: seller findByAll ===");
 		list = sellerDao.findAll();
-		for(Seller obj: list) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-	}
 
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "João", "joao@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+
+	}
 }
